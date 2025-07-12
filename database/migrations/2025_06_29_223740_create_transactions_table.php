ad00e4dfbd6d2b14fc8c17b9f6ddaba1108f8f15
alexisdjgoyo <alexisdjesusgoyoflores@gmail.com>
2025-07-12 19:12:46 -0400

    Nueva columna date en la tabla transactions

--- database/migrations/2025_06_29_223740_create_transactions_table.php
+++ database/migrations/2025_06_29_223740_create_transactions_table.php
@@ -16,8 +16,9 @@ return new class extends Migration
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->foreignId('category_id')->constrained()->onDelete('cascade');
             $table->decimal('amount', 10, 2);
-            $table->string('description',500)->nullable();
+            $table->string('description', 500)->nullable();
             $table->string('image', 420)->nullable();
+            $table->date('date')->nullable();
             $table->timestamps();
         });
     }
